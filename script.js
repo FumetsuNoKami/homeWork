d24282310f102cae6fccd658deb9a57c8225dd45
Andrew <Andrey_U_O@mail.ru>
2022-09-04 17:20:14 +0300

    Last commit upd

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", '');
 
 const  personalMovieDB = {
     count: numberOfFilms,
@@ -8,14 +8,14 @@ const  personalMovieDB = {
     private: false
 };
 
-let filmTitle = prompt('Один из последних просмотренных фильмов?');
-let filmRating = prompt('На сколько оцените его?');
+let filmTitle = prompt('Один из последних просмотренных фильмов?','');
+let filmRating = prompt('На сколько оцените его?','');
 
 personalMovieDB.movies[filmTitle] = filmRating;
-console.log(personalMovieDB.movies);
 
-filmTitle = prompt('Один из последних просмотренных фильмов?');
-filmRating = prompt('На сколько оцените его?');
+filmTitle = prompt('Один из последних просмотренных фильмов?','');
+filmRating = prompt('На сколько оцените его?','');
 
 personalMovieDB.movies[filmTitle] = filmRating ;
-console.log(personalMovieDB.movies);
+
+console.log(personalMovieDB);

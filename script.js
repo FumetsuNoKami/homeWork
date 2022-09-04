ec2d4d2e74dea2b4ac0810989aa24ae60bbda3c7
Andrew <Andrey_U_O@mail.ru>
2022-09-04 17:14:20 +0300

    Add keys to movies

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?")
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
 
 const  personalMovieDB = {
     count: numberOfFilms,
@@ -6,4 +6,16 @@ const  personalMovieDB = {
     actors: {},
     genres: [],
     private: false
-}
+};
+
+let filmTitle = prompt('Один из последних просмотренных фильмов?');
+let filmRating = prompt('На сколько оцените его?');
+
+personalMovieDB.movies[filmTitle] = filmRating;
+console.log(personalMovieDB.movies);
+
+filmTitle = prompt('Один из последних просмотренных фильмов?');
+filmRating = prompt('На сколько оцените его?');
+
+personalMovieDB.movies[filmTitle] = filmRating ;
+console.log(personalMovieDB.movies);
